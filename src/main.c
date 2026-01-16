6e0f932a7d5a6ffaf2487c00da1a84d6c032353a
Jayesh Puri <jayeshkpuri@gmail.com>
2026-01-16 16:46:41 +0530

    basic done - working

--- src/main.c
+++ src/main.c
@@ -8,83 +8,109 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define TARGET_FPS 60
+#define pixel_w 10
+#define pixel_h 10
+
+typedef struct{
+  uint8_t color_r;
+  uint8_t color_g;
+  uint8_t color_b;
+  uint8_t color_a;
+} Color;
+
+const float_t delay_time = (1.0 / TARGET_FPS); // in milliseconds
+
+Color red = {255, 0, 0, 255};
+Color green = {0, 255, 0, 255};
+Color blue = {0, 0, 255, 255};
+Color black = {0, 0, 0, 255};
+Color white = {255, 255, 255, 255};
+
+Color C_arr[5] = {
+  {255, 0, 0, 255},   // Red
+  {0, 255, 0, 255},   // Green
+  {0, 0, 255, 255},   // Blue
+  {0, 0, 0, 255},     // Black
+  {255, 255, 255, 255}, // White
+  {255, 255, 0, 255}  //yellow 
+};
+
+void drawPixel(bool draw, int _x, int _y, SDL_Surface *psurf, SDL_Window *pwindow, Color C_used){
+  
+  SDL_Rect pixel = (SDL_Rect){_x-pixel_w/2, _y-pixel_h/2, pixel_w, pixel_h}; // x and y are specified and width and height is specified as 1 pixel each
+  // Clear screen with black color
+  SDL_FillRect(psurf, &pixel, SDL_MapRGB(psurf->format, C_used.color_r, C_used.color_g, C_used.color_b));
+
+  // Here you can add your drawing code
+
+
+}
+
 
 int main(int argc, char *argv[]) {
   bool done = false;
-  float_t delay_time = (1.0 / TARGET_FPS); // in milliseconds
 
   printf("SDL2 Image Viewer\n");
-
+  
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window *pwindow =
-      SDL_CreateWindow("Paint C", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-  
+  SDL_CreateWindow("Paint C", SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    
   SDL_Surface *psurf = SDL_GetWindowSurface(pwindow);
   SDL_FillRect(psurf, NULL, SDL_MapRGB(psurf->format, 0, 0, 0));
   SDL_UpdateWindowSurface(pwindow);
   bool draw = false;
   int _x, _y;
-  int32_t pixel_w = 20;
-  int32_t pixel_h = 20;
+  Color CC_used = black;
+  int i = 0; 
   while(!done){
+    uint32_t frame_start = SDL_GetTicks();
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       switch(event.type){
         case SDL_QUIT:
           done = true;
           break;
+        case SDL_MOUSEBUTTONDOWN:
+          if (event.button.button == SDL_BUTTON_LEFT) {
+            draw = true;
+            _x = event.button.x;
+            _y = event.button.y;
+            CC_used = C_arr[i%6];
+          }
+          else if (event.button.button == SDL_BUTTON_RIGHT) {
+            draw = true;
+            _x = event.button.x;
+            _y = event.button.y;
+            i++;
+          }
+          break;
+        case SDL_MOUSEBUTTONUP:
+          // CC_used = black;
+          draw = false;
+          break;  
+          if (event.button.button == SDL_BUTTON_LEFT) {
+              draw = false;
+          }
+          break;
         case SDL_MOUSEMOTION:
           _x = event.motion.x;
           _y = event.motion.y;
-          draw = true;
-          
+          break;
       }
 
     }
-    if(draw){
-      SDL_Rect pixel = (SDL_Rect){_x-pixel_w/2, _y-pixel_h/2, pixel_w, pixel_h}; // x and y are specified and width and height is specified as 1 pixel each
-      // Clear screen with black color
-      SDL_FillRect(psurf, &pixel, SDL_MapRGB(psurf->format, 255, 0, 0));
-
-      // Here you can add your drawing code
-
-      SDL_UpdateWindowSurface(pwindow);
-      SDL_Delay((uint32_t)(delay_time * 1000)); // Convert to milliseconds
+    if(draw){drawPixel(draw, _x, _y, psurf, pwindow, CC_used); 
+    SDL_UpdateWindowSurface(pwindow);
+    }
+    uint32_t frame_time = SDL_GetTicks() - frame_start;
+    if(frame_time < (uint32_t)(delay_time * 1000)){
+      SDL_Delay((uint32_t)(delay_time * 1000) - frame_time);
+      // Convert to milliseconds
     }
   }
-  // Init Pixel
-  //   int x, y;
-  //   SDL_Rect pixel =
-  //       (SDL_Rect){0, 0, 1, 1}; // x and y are specified and width and height
-  //       is
-  //                               // specified as 1 pixel each
-
-  //   for (x = 0; x < width; x++) {
-  //     for (y = 0; y < height; y++) {
-  //       int idx = (y * loadedImg.width + x) * 3;
-  //       uint8_t r = loadedImg.body[idx];
-  //       uint8_t g = loadedImg.body[idx + 1];
-  //       uint8_t b = loadedImg.body[idx + 2];
-  //       uint32_t color = SDL_MapRGB(psurf->format, r, g, b);
-  //       pixel.x = x;
-  //       pixel.y = y;
-  //       SDL_FillRect(psurf, &pixel, color);
-  //     }
-  //   }
-  //   SDL_UpdateWindowSurface(pwindow);
-
-  //   uint32_t start = SDL_GetTicks();
-  //   SDL_Event e;
-
-  //   while (SDL_GetTicks() - start < delay_time) {
-  //       while (SDL_PollEvent(&e)) {
-  //           if (e.type == SDL_QUIT) {
-  //               SDL_DestroyWindow(pwindow);
-  //               SDL_Quit();
-  //               free(loadedImg.body);
-  //               return 0;
-  //           }
-  //       }
-  //   }
+  SDL_DestroyWindow(pwindow);
+  SDL_Quit();
+  return 0;
 }
